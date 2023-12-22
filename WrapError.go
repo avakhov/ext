da8df87d9066477aa1009615165669358ac655ba
Alexey Vakhov <vakhov@gmail.com>
2023-12-23 01:40:43 +0300

    Create WrapError.go

---
+++ WrapError.go
@@ -0,0 +1,29 @@
+package util
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"runtime"
+	"strings"
+)
+
+func WrapError(in interface{}, a ...any) error {
+	_, full, line, _ := runtime.Caller(1)
+	root, err := os.Getwd()
+	if err != nil {
+		return errors.New("sys error:" + err.Error())
+	}
+	file := strings.Replace(full, root+"/", "", 1)
+	prefix := fmt.Sprintf("%s:%d →\n", file, line)
+	switch v := in.(type) {
+	case error:
+		return errors.New(prefix + v.Error())
+	case string:
+		return errors.New(fmt.Sprintf(prefix+v, a...))
+	case nil:
+		return nil
+	default:
+		return errors.New(prefix + " unknown in type")
+	}
+}
